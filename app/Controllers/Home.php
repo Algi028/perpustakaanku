1bcd5e0140b905cc8d21de2eef69c930e851e027
Algi028 <giwalgi028@gmail.com>
2023-05-11 11:46:45 +0700

    komit ketiga

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -5,14 +5,20 @@ namespace App\Controllers;
 class Home extends BaseController
 {
     public function index()
-    {
-        return view('welcome_message');
+    { 
+        if(session('id'))
+        {
+            return redirect()->to(base_url('home'));
+        }
+        return view('login');
     }
 
     public function home()
     {
+        if(!session('id'))
+        {
+            return redirect()->to(base_url())->with('error', 'Anda harus Login');
+        }
         return view('home');
     }
-
-
 }
